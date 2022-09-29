0f36cb49f8c0154a0f7bd733fe5796f502ce0672
Garo24 <113709968+Garo24@users.noreply.github.com>
2022-09-29 17:44:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿char caracter;
+caracter = 'a';
+caracter = 'e';
+caracter = 'i';
+caracter = 'o';
+caracter = 'u';
+do
+{
+    Console.Write("INGRESE UN CARACTER: ");
+    caracter = char.Parse(Console.ReadLine());
+}
+while (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u'&&
+caracter != 'A' && caracter != 'E' && caracter != 'I' && caracter != 'O' && caracter != 'U');
+ Console.Write("LA PRIMERA VOCAL ES: : "+ caracter);
