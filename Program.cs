601ff4897fee669d9289e146c2ff38242e53999e
Garo24 <113709968+Garo24@users.noreply.github.com>
2022-09-29 17:50:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int num1, num2;
+Console.Write("INGRESE NUMERO DE FILAS: ");
+num1 = int.Parse(Console.ReadLine());
+Console.Write("INGRESE NUMERO DE COLUMNAS: ");
+num2 = int.Parse(Console.ReadLine());
+
+
+
+for (int i = 1; i <= num1; i++)
+{
+    for (int j = 1; j <= num2; j++)
+    {
+        Console.Write("+ ");
+    }
+    Console.WriteLine("");
+}
+
+
+Console.ReadKey();
